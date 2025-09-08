5e4474d7531b3c15241fc185bcfcc69cc9878281
RKSANJANA <krandhi@gitam.in>
2025-09-08 09:21:49 +0000

    Added my codes

---
+++ waterfall_mode.py
@@ -0,0 +1,6 @@
+from weather_model import quadratic_weather_model
+
+print("=== WATERFALL MODE ===")
+for hour in range(0, 25, 6):
+    temp = quadratic_weather_model(hour)
+    print(f"TIme: {hour} hrs -> Predicted Temp: {temp:.2f}°C")
